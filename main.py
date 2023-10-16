62a3844386d22b9a20280fddedd930fa67f470a1
Valentyn Tonkonih <xzibitness@gmail.com>
2023-10-16 15:58:24 +0300

    add types

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from collections import UserDict
 
 
 class Field:
-    def __init__(self, value):
+    def __init__(self, value: str) -> None:
         self.value = value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.value)
 
 
 class Name(Field):
-    def __init__(self, value):
+    def __init__(self, value: str) -> None:
         super().__init__(value)
 
 
 class Phone(Field):
-    def __init__(self, value: str) -> None:
+    def __init__(self, value: str) -> None | str:
         if len(value) == 10 and value.isdigit():
             super().__init__(value)
         else:
@@ -23,14 +23,14 @@ class Phone(Field):
 
 
 class Record:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = Name(name)
         self.phones = []
 
-    def add_phone(self, phone):
+    def add_phone(self, phone: str) -> None:
         self.phones.append(Phone(phone))
     
-    def remove_phone(self, phone_number):
+    def remove_phone(self, phone_number: str) -> None | str:
         for phone in self.phones:
             if phone.value == phone_number:
                 self.phones.remove(phone)
@@ -38,7 +38,7 @@ class Record:
         else:
             raise ValueError(f'phone {phone_number} not found in the record')
         
-    def edit_phone(self, old_phone, new_phone) -> None | str:
+    def edit_phone(self, old_phone: str, new_phone: str) -> None | str:
         for id, phone in enumerate(self.phones):
             if phone.value == old_phone:
                 self.phones[id] = Phone(new_phone)
@@ -46,25 +46,25 @@ class Record:
         else:
             raise ValueError(f'phone {old_phone} not found in the record')        
         
-    def find_phone(self, phone_number):
+    def find_phone(self, phone_number: str) -> Phone | str:
         for phone in self.phones:
             if phone.value == phone_number:
                 return phone
         else:
             raise ValueError(f'phone {phone_number} not found in the record')          
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"    
 
 
 class AddressBook(UserDict):
-    def add_record(self, record):
+    def add_record(self, record: Record) -> None:
         self.data[record.name.value] = record
         
-    def find(self, name):
+    def find(self, name: str) -> str:
         return self.data[name]
 
-    def delete(self, name):
+    def delete(self, name: str) -> None:
         self.data.pop(name)
 
 
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     book.add_record(john_record)
     john = book.find('John')
     #print (john)
-    
+
     jane_record = Record('Jane')
     jane_record.add_phone('3333333333')
     jane_record.add_phone('4444444444')
